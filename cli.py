8c37dfc03d9df8e5f049945b90390007c5f74dbc
kyakei <codtool911@gmail.com>
2025-03-19 07:45:27 +0530

    Initial commit

---
+++ cli.py
@@ -0,0 +1,8 @@
+from core.banner import funky_loading
+from core.loader import loading_animation
+from core.main_menu import main_menu  # Now it works!
+
+if __name__ == "__main__":
+    funky_loading()
+    loading_animation()
+    main_menu()
