ae1f6d4efa7d10ddefc734315fe63b5c47e203ee
kyakei <codtool911@gmail.com>
2025-04-09 11:36:54 +0530

    Update cli.py to fix executable path

--- cli.py
+++ cli.py
@@ -1,8 +1,20 @@
-from core.banner import funky_loading
-from core.loader import loading_animation
-from core.main_menu import main_menu  # Now it works!
+#!/usr/bin/env python3
 
-if __name__ == "__main__":
+import os
+import sys
+
+# Add the project root directory to the sys.path
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+# Import functions from the kyakx module
+from kyakx.core.banner import funky_loading
+from kyakx.core.loader import loading_animation
+from kyakx.core.main_menu import main_menu
+
+def main():
     funky_loading()
     loading_animation()
     main_menu()
+
+if __name__ == "__main__":
+    main()
