f9a885d49afbc76c13f24481fb097a2c0c12c633
kyakei <codtool911@gmail.com>
2025-04-09 11:40:34 +0530

    Remove email from version information for privacy

--- kyakx/core/version.py
+++ kyakx/core/version.py
@@ -2,13 +2,13 @@
 
 __version__ = "1.2.0"
 __author__ = "kyakei"
-__email__ = "codtool911@gmail.com"
+# Email removed for privacy
 __description__ = "A powerful web exploitation toolkit"
 
 def get_version_info():
     """Return formatted version information."""
     return f"""
 KyakX v{__version__}
-Created by: {__author__} <{__email__}>
+Created by: {__author__}
 {__description__}
 """
